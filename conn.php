3173e932540f0f6b37ef98ffa5dc60f1680c1898
ronicairo <roni.cairo@outlook.fr>
2023-07-14 17:50:13 +0200

    final

--- conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$conn = mysqli_connect('phpmyadmin.viaduc.fr', 'h20465', '8parDX3I', 'h20465_amyazvocab') or die(mysqli_error());
-	
-	if(!$conn){
-		die("Error: Failed to connect to database");
-	}
-?>
